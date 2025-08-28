f39ea46d0e5af0ba9ce08738e4d35816ec7dc726
cirivere <147147650+cirivere@users.noreply.github.com>
2025-08-28 15:44:33 +0200

    Update script.js
    added console log

--- script.js
+++ script.js
@@ -283,6 +283,7 @@ function saveInputData(tribe) {
 
 window.onload = function() {
  const AllInputss = document.getElementsByClassName("input");
+ console.log();
  For (let i = 0;i < AllInputs.length; i++) {
   AllInputs[i].AddEventListener("change" function() {
    const tribe = this.id.split("_")[0]; //get tribe name from input field ID
